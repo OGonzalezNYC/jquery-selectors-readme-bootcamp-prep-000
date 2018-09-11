8fdd4d1c4d947c3ba0328dcac85a2054bb80846e
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-11 20:15:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -17,14 +17,9 @@ function divSelector() {
 }
 
 function firstListItem() {
-<<<<<<< HEAD
   return $("ul#pic-list li:first-child")
 }
 
 //function firstListItem() {
 //  return $("ul li:first-child")
 //}
-=======
-  return $("ul #pic-list:first-child")
-}
->>>>>>> feb5afcf803da24690602c0771fe02a402aeb601
